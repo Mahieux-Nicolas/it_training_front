8ba7a4eb1a54dfa0a927788ecf66f1d3ed9fc82e
mBou <bourzizmehdi@gmail.com>
2023-08-03 16:22:47 +0200

    feat/ Ajout de la création du projet angular + création du composant navbar

---
+++ src/app/commons/nav-bar/nav-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent {
+
+  recherche = new FormControl('');
+
+  onSubmit(){
+
+  }
+
+}
